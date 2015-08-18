abc00ab43d160dfcd9e86082d6d968e7b16108b5
kgryte <kgryte@gmail.com>
2015-08-18 00:46:37 -0700

    [TESTS] refactor tests to accommodate how typed arrays are handled on v0.10 and v0.8.

--- test/test.array.js
+++ test/test.array.js
@@ -134,22 +134,36 @@ describe( 'array', function tests() {
 	});
 
 	it( 'should cast an input array to a specified type', function test() {
-		var arr;
+		var expected,
+			arr,
+			i;
 
 		arr = array( new Int8Array( 10 ), 'uint16' );
 		assert.strictEqual( arr.constructor.name, 'Uint16Array' );
+
 		assert.strictEqual( arr.BYTES_PER_ELEMENT, 2 );
-		assert.deepEqual( arr, new Uint16Array( 10 ) );
+		expected = new Uint16Array( 10 );
+		for ( i = 0; i < arr.length; i++ ) {
+			assert.strictEqual( arr[ i ], expected[ i ] );
+		}
 
 		arr = array( new Float32Array( 10 ), 'uint8_clamped' );
 		assert.strictEqual( arr.constructor.name, 'Uint8ClampedArray' );
 		assert.strictEqual( arr.BYTES_PER_ELEMENT, 1 );
-		assert.deepEqual( arr, new Uint8ClampedArray( 10 ) );
+
+		expected = new Uint8ClampedArray( 10 );
+		for ( i = 0; i < arr.length; i++ ) {
+			assert.strictEqual( arr[ i ], expected[ i ] );
+		}
 
 		arr = array( new Uint32Array( [1,2,3,4] ), 'float32' );
 		assert.strictEqual( arr.constructor.name, 'Float32Array' );
 		assert.strictEqual( arr.BYTES_PER_ELEMENT, 4 );
-		assert.deepEqual( arr, new Uint32Array( [1,2,3,4] ) );
+
+		expected = new Uint32Array( [1,2,3,4] );
+		for ( i = 0; i < arr.length; i++ ) {
+			assert.strictEqual( arr[ i ], expected[ i ] );
+		}
 	});
 
 	it( 'should cast a plain array to a float64', function test() {
