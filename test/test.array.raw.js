03777e4c3885dce2d445341fe76914ce888c2e60
kgryte <kgryte@gmail.com>
2015-12-15 17:13:36 -0800

    Fix broken tests by using instanceof rather than constructor name

--- test/test.array.raw.js
+++ test/test.array.raw.js
@@ -25,11 +25,11 @@ describe( 'array (raw)', function tests() {
 		var arr;
 
 		arr = array( 1 );
-		assert.strictEqual( arr.constructor.name, 'Float64Array' );
+		assert.isTrue( arr instanceof Float64Array );
 		assert.deepEqual( arr, new Float64Array( 1 ) );
 
 		arr = array( 1, 'int8' );
-		assert.strictEqual( arr.constructor.name, 'Int8Array' );
+		assert.isTrue( arr instanceof Int8Array );
 		assert.deepEqual( arr, new Int8Array( 1 ) );
 	});
 
@@ -39,7 +39,7 @@ describe( 'array (raw)', function tests() {
 		var i;
 
 		arr = array( new Int8Array( 10 ), 'uint16' );
-		assert.strictEqual( arr.constructor.name, 'Uint16Array' );
+		assert.isTrue( arr instanceof Uint16Array );
 
 		assert.strictEqual( arr.BYTES_PER_ELEMENT, 2 );
 		expected = new Uint16Array( 10 );
@@ -48,7 +48,7 @@ describe( 'array (raw)', function tests() {
 		}
 
 		arr = array( new Float32Array( 10 ), 'uint8_clamped' );
-		assert.strictEqual( arr.constructor.name, 'Uint8ClampedArray' );
+		assert.isTrue( arr instanceof Uint8ClampedArray );
 		assert.strictEqual( arr.BYTES_PER_ELEMENT, 1 );
 
 		expected = new Uint8ClampedArray( 10 );
@@ -57,7 +57,7 @@ describe( 'array (raw)', function tests() {
 		}
 
 		arr = array( new Uint32Array( [1,2,3,4] ), 'float32' );
-		assert.strictEqual( arr.constructor.name, 'Float32Array' );
+		assert.isTrue( arr instanceof Float32Array );
 		assert.strictEqual( arr.BYTES_PER_ELEMENT, 4 );
 
 		expected = new Uint32Array( [1,2,3,4] );
@@ -69,15 +69,16 @@ describe( 'array (raw)', function tests() {
 	it( 'should cast a plain array to a float64', function test() {
 		var out;
 		var arr;
+		var i;
 
 		arr = new Array(1,2,3,4,5,6);
 		out = array( arr );
 
-		assert.strictEqual( out.constructor.name, 'Float64Array' );
+		assert.isTrue( out instanceof Float64Array );
 		assert.strictEqual( out.BYTES_PER_ELEMENT, 8 );
 		assert.strictEqual( out.length, arr.length );
 
-		for ( var i = 0; i < arr.length; i++ ) {
+		for ( i = 0; i < arr.length; i++ ) {
 			assert.strictEqual( arr[ i ], out[ i ] );
 		}
 	});
