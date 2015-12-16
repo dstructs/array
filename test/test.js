1d87b3bd4e1730eb1b7a86a19c8974da478a2b2c
kgryte <kgryte@gmail.com>
2015-12-15 20:38:20 -0800

    Move dtypes to separate file and add tests for class support in main file

--- test/test.js
+++ test/test.js
@@ -1,15 +1,20 @@
 /* global require, describe, it */
 'use strict';
 
+var mpath = './../lib';
+
+
 // MODULES //
 
 var chai = require( 'chai' );
-var array = require( './../lib' );
+var proxyquire = require( 'proxyquire' );
+var array = require( mpath );
 
 
 // VARIABLES //
 
 var expect = chai.expect;
+var assert = chai.assert;
 
 
 // TESTS //
@@ -22,6 +27,40 @@ describe( 'dstructs-array', function tests() {
 
 	it( 'should export a function which provides fewer guarantees when validating input arguments', function test() {
 		expect( array.raw ).to.be.a( 'function' );
+	});
+
+	it( 'should use classes when classes are supported', function test() {
+		var array = proxyquire( mpath, {
+			'./detect.js': detect,
+			'./array.js': fcn,
+			'./array.raw.js': raw
+		});
+
+		assert.strictEqual( array(), 'beep' );
+		assert.strictEqual( array.raw(), 'boop' );
+
+		function detect() {
+			return true;
+		}
+		function fcn() {
+			return 'beep';
+		}
+		function raw() {
+			return 'boop';
+		}
+	});
+
+	it( 'should fallback to polyfills if `class` is not supported', function test() {
+		var array = proxyquire( mpath, {
+			'./detect.js': detect
+		});
+
+		expect( array ).to.be.a( 'function' );
+		expect( array.raw ).to.be.a( 'function' );
+
+		function detect() {
+			return false;
+		}
 	});
 
 });
